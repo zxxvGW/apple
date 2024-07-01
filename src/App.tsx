7677987329151c4a8c697fe216c1045ee7a3784c
zxxv <2282586126@qq.com>
2024-07-01 13:48:28 +0800

    feat: complete navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import { useState } from "react";
-import "./App.css";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <main className="bg-black">
+        <NavBar />
+        <Hero />
+        <Highlights />
+      </main>
+    </>
+  );
 }
 
 export default App;
